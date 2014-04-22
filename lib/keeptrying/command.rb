acb6fd2497af9c10dce477a20f887cee072a8f1c
Jun Yokoyama <jun@larus.org>
2014-04-22 23:00:34 +0900

    テストを追加し、コマンドラインオプションを整理した

--- lib/keeptrying/command.rb
+++ lib/keeptrying/command.rb
@@ -8,58 +8,44 @@ module Keeptrying
       @argv = argv
     end
 
-    def parse_args
-      @action = @argv[0] || 'recent'
-
-      unless @argv[1] == '-a'
-        { keep: '-k', problem: '-p', try: '-t' }.each do |k, v|
-          if @argv[1] == v
-            @tag = k
-          end
-        end
+    def execute
+      parse_args
+      parse_days
+      case @action
+      when 'show', 's'
+        show
+      when 'recent', 'r'
+        show
+      when 'all', 'a'
+        all
+      when 'write', 'w'
+        write
+      when 'done', 'd'
+        done
+      when 'truncate', 't'
+        truncate
+      when 'help', 'h'
+        help
       end
     end
 
-    def parse_days
-      @today = Date.today
-      @days = @argv[2] || 7
-      @to = Time.now
-      @from = (Date.today - @days.to_i).to_time
-    end
-
-    def parse_body
-      @body = @argv[2]
-    end
-
-    def display(list)
-      if list.empty?
-        puts "database is empty."
-      else
-        list.each do |entry|
-          puts "#{Time.at(entry[:utc])} #{colored_tag(entry[:tag_id])} #{entry[:body]}#{' (done)' if entry[:done] > 0}"
-        end
-      end
-    end
-
-    def show_list
-      display kpt.get
-    end
+    private
 
     def show
       parse_days
-      kpt.select @from, @to, @tag
-      show_list
+      kpt.query @from, @to, @tag
+      output kpt.get
     end
 
     def all
       parse_days
-      kpt.select @from, @to, @tag, true
-      show_list
+      kpt.query @from, @to, @tag, true
+      output kpt.get
     end
 
     def write
       if @tag
-        if (@argv[2])
+        if @argv[2]
           kpt.write @tag, @argv[2]
         else
           tempfile.close false
@@ -74,32 +60,77 @@ module Keeptrying
     end
 
     def done
-      @days = @argv[2].to_i
+      @days = @argv[1].to_i
       if @days > 0
-        kpt.select 0, (Date.today - @days.to_i).to_time
+        kpt.query @from, @to
       else
-        kpt.select
+        kpt.query
       end
       kpt.done
     end
 
-    def execute
-      parse_args
-      case @action
-      when 'show'
-        show
-      when 'recent'
-        show
-      when 'all'
-        all
-      when 'write'
-        write
-      when 'done'
-        done
+    def truncate
+      @days = @argv[1].to_i
+      if @days > 0
+        kpt.query @from, @to
+        kpt.truncate
       end
     end
 
-    private
+    def help
+      puts <<-EOS
+      usage:
+      kpt show [-k|p|t] [days] (or kpt s)
+        show KPT entries
+      kpt write -k|p|t [message] (or kpt w)
+        write a KPT entry (if omit message, open editor)
+      kpt done [days] (or kpt d)
+        entries are marked with "done" flag.
+      kpt all [-k|-p|-t] [days] (or kpt a)
+        show entries (includes "done" entries)
+      kpt truncate days (or kpt t)
+        delete old entries
+      EOS
+    end
+
+    def parse_days
+      @today = Date.today
+      if %w(done truncate).include?(@action)
+        @days = @argv[1].to_i
+        @from = 0
+        @to = ago(@days)
+      else
+        @days = @argv[2] || 7
+        @to = Time.now
+        @from = ago(@days)
+      end
+    end
+
+    def parse_args
+      @action = @argv[0] || 'show'
+
+      unless @argv[1] == '-a'
+        { keep: '-k', problem: '-p', try: '-t' }.each do |k, v|
+          if @argv[1] == v
+            @tag = k
+          end
+        end
+      end
+    end
+
+    def parse_body
+      @body = @argv[2]
+    end
+
+    def output(list)
+      if list.empty?
+        puts "database is empty."
+      else
+        list.each do |entry|
+          puts "#{Time.at(entry[:utc])} #{colored_tag(entry[:tag_id])} #{entry[:body]}#{' (done)' if entry[:done] > 0}"
+        end
+      end
+    end
 
     def tempfile
       @tempfile ||= Tempfile.open('kpt')
@@ -116,6 +147,10 @@ module Keeptrying
     def colored_tag(id)
       color = [:blue, :red, :yellow]
       kpt.tags[id].to_s.send(color[id])
+    end
+ 
+    def ago(days)
+      (Date.today - days.to_i).to_time
     end
   end
 end
--- spec/kpt_spec.rb
+++ spec/kpt_spec.rb
@@ -6,102 +6,201 @@ describe "Keeptrying" do
 
   let(:db_file) { "./kpt_test.sql" }
   let(:tags) { [:keep, :problem, :try] }
+  let(:tag) { tags.sample }
   let(:sentence) { Faker::Lorem.sentence }
   let(:table) { Keeptrying::Kpt.db[:entries] }
   let(:all_entries) { table.all }
   let(:past_time) { Time.local(2014, 4, 1, 10, 0, 0) }
   let(:recent_time) { Time.local(2014, 4, 3, 10, 0, 0) }
+  let(:now) { Time.local(2014, 4, 4, 0, 0, 0) }
+  let(:sentences) { 3.times.map { Faker::Lorem.sentence } }
+
+  before do
+    Keeptrying::Kpt.stub(:db_file).and_return(db_file)
+    @kpt = Keeptrying::Kpt.new
+  end
+
+  after(:all) do
+    File.delete db_file
+  end
 
   describe "Kpt" do
-    describe "initialize" do
+    after do
+      table.truncate
+    end
+
+    it "dbが存在しない場合に作成されること" do
+      File.should be_exists(db_file)
+    end
+
+    it "writeで内容が書き込まれること" do
+      @kpt.write tag, sentence
+      all_entries.last[:body].should eq(sentence)
+    end
+
+    it "getで内容が取得できること" do
+      @kpt.write tag, sentence
+      @kpt.query
+      @kpt.get.last[:body].should eq(sentence)
+    end
+
+    it "doneで処理済みになること" do
+      create_entry
+      @kpt.query
+      @kpt.done
+      all_entries.last[:done].should eq(1)
+    end
+
+    it "タグを指定して内容が取得できること" do
+      sentences = 5.times.map{ random_sentence }
+      sentences[0, 3].each { |sentence| @kpt.write(:keep, sentence) }
+      sentences[3, 2].each { |sentence| @kpt.write(:problem, sentence) }
+      @kpt.query nil, nil, :keep
+      @kpt.get.map{ |x| x[:body] }.should =~ sentences[0,3]
+    end
+
+    describe "collections by timeline" do
       before do
-        Keeptrying::Kpt.stub(:db_file).and_return(db_file)
-        @kpt = Keeptrying::Kpt.new
+        @sentences = 5.times.map { random_sentence }
+        Timecop.freeze(past_time) do
+          @sentences[0, 3].each { |sentence| create_entry(sentence,) }
+        end
+
+        Timecop.freeze(recent_time) do
+          @sentences[3, 2].each { |sentence| create_entry(sentence) }
+        end
       end
 
-      after do
-        table.truncate
-      end
-
-      after(:all) do
-        File.delete db_file
-      end
-
-      it "dbが存在しない場合に作成されること" do
-        File.should be_exists(db_file)
-      end
-
-      it "writeで内容が書き込まれること" do
-        create_entry sentence
-        all_entries.last[:body].should eq(sentence)
-      end
-
-      it "getで内容が取得できること" do
-        create_entry sentence
+      it "getで指定した件数分の内容が取得できること" do
         @kpt.query
-        @kpt.get.last[:body].should eq(sentence)
+        @kpt.get(2).all.size.should eq(2)
       end
 
-      it "doneで処理済みになること" do
-        create_entry
-        @kpt.query
-        @kpt.done
+      it "時間を指定して内容が取得できること" do
+        @kpt.query((past_time.to_i - 60), (recent_time.to_i - 60))
+        @kpt.get.map{ |x| x[:body] }.should =~ @sentences[0, 3]
+      end
+
+      describe "processed entries" do
+        before do
+          @kpt.query((past_time.to_i - 60), (recent_time.to_i - 60))
+          @kpt.done
+        end
+
+        it "期間を指定して処理済みになること" do
+          all_entries.select{ |x| x[:done] > 0 }.
+            map{ |x| x[:body] }.should =~ @sentences[0, 3]
+        end
+
+        it "処理済みの内容が取得されないこと" do
+          kpt_after = Keeptrying::Kpt.new
+          kpt_after.query
+          kpt_after.get.map{ |x| x[:body] }.should =~ @sentences[3, 2]
+        end
+
+        it "処理済みを含めた内容を取得できること" do
+          kpt_after = Keeptrying::Kpt.new
+          kpt_after.query nil, nil, nil, true
+          kpt_after.get.size.should eq(5)
+        end
+      end
+    end
+  end
+
+  describe "Command" do
+    include FixtureHelpers
+    include CaptureHelpers
+
+    let(:command) { Keeptrying::Command }
+
+    after do
+      table.truncate
+    end
+
+    it "writeで内容が書き込まれること" do
+      command.run ['write', "-#{tag[0]}", sentence]
+      all_entries.last[:body].should eq(sentence)
+    end
+
+    it "showで内容が表示されること" do
+      command.run ['write', "-#{tag[0]}", sentence]
+      capture(:stdout) { command.run ['show'] }.should be_include(sentence) 
+    end
+
+    it "showで特定のタグの内容が表示されること" do
+      command.run ['write', "-k", sentences[0]]
+      command.run ['write', "-p", sentences[1]]
+      command.run ['write', "-t", sentences[2]]
+      capture(:stdout) {
+        command.run ['show', '-k']
+      }.should be_include(sentences[0]) 
+      capture(:stdout) {
+        command.run ['show', '-k']
+      }.should_not be_include(sentences[1]) 
+    end
+
+    describe "day range" do
+      before do
+        Timecop.freeze(past_time) do
+          command.run ['write', "-k", sentences[0]]
+          command.run ['write', "-p", sentences[1]]
+        end
+        Timecop.freeze(recent_time) do
+          command.run ['write', "-t", sentences[2]]
+        end
+      end
+
+      it "showで特定の日数以内の内容が表示されること" do
+        Timecop.freeze(now) do
+          capture(:stdout) {
+            command.run ['show', '-t', 1]
+          }.should be_include(sentences[2]) 
+          capture(:stdout) {
+            command.run ['show', '-k', 1]
+          }.should_not be_include(sentences[0]) 
+        end
+      end
+
+      it "doneで全ての内容が処理済みになること" do
+        command.run ['done']
         all_entries.last[:done].should eq(1)
       end
 
-      it "タグを指定して内容が取得できること" do
-        sentences = 5.times.map{ random_sentence }
-        sentences[0, 3].each { |sentence| @kpt.write(:keep, sentence) }
-        sentences[3, 2].each { |sentence| @kpt.write(:problem, sentence) }
-        @kpt.query nil, nil, :keep
-        @kpt.get.map{ |x| x[:body] }.should =~ sentences[0,3]
+      it "showで処理済みの内容が表示されないこと" do
+        command.run ['done']
+        capture(:stdout) {
+          command.run ['show', '-t', 1]
+        }.should_not be_include(sentences[2]) 
+        capture(:stdout) {
+          command.run ['show', '-k', 1]
+        }.should_not be_include(sentences[0]) 
       end
 
-      describe "collections by timeline" do
-        before do
-          @sentences = 5.times.map { random_sentence }
-          Timecop.freeze(past_time) do
-            @sentences[0, 3].each { |sentence| create_entry(sentence,) }
-          end
-
-          Timecop.freeze(recent_time) do
-            @sentences[3, 2].each { |sentence| create_entry(sentence) }
-          end
+      it "doneで特定の日数以上の内容が処理済みになること" do
+        Timecop.freeze(now) do
+          command.run ['done', 1]
         end
+        all_entries.first[:done].should eq(1)
+      end
 
-        it "getで指定した件数分の内容が取得できること" do
-          @kpt.query
-          @kpt.get(2).all.size.should eq(2)
+      it "allで処理済みの内容も取得できること" do
+        command.run ['done']
+        capture(:stdout) {
+          command.run ['all']
+        }.should_not be_include(sentences[0]) 
+      end
+
+      it "truncateで特定の日数以上の処理済みの内容が削除されること" do
+        Timecop.freeze(now) do
+          command.run ['truncate', 1]
         end
+        all_entries.count.should eq(1)
+      end
 
-        it "時間を指定して内容が取得できること" do
-          @kpt.query((past_time.to_i - 60), (recent_time.to_i - 60))
-          @kpt.get.map{ |x| x[:body] }.should =~ @sentences[0, 3]
-        end
-
-        describe "processed entries" do
-          before do
-            @kpt.query((past_time.to_i - 60), (recent_time.to_i - 60))
-            @kpt.done
-          end
-
-          it "期間を指定して処理済みになること" do
-            all_entries.select{ |x| x[:done] > 0 }.
-              map{ |x| x[:body] }.should =~ @sentences[0, 3]
-          end
-
-          it "処理済みの内容が取得されないこと" do
-            kpt_after = Keeptrying::Kpt.new
-            kpt_after.query
-            kpt_after.get.map{ |x| x[:body] }.should =~ @sentences[3, 2]
-          end
-
-          it "処理済みを含めた内容を取得できること" do
-            kpt_after = Keeptrying::Kpt.new
-            kpt_after.query nil, nil, nil, true
-            kpt_after.get.size.should eq(5)
-          end
-        end
+      it "helpでヘルプが表示されること" do
+        capture(:stdout) {
+          command.run ['help']
+        }.should be_include('usage') 
       end
     end
   end
